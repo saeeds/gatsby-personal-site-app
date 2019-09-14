0979125883d317c437a24199e44a07a7c9a178a4
Saeed Alsharqawi <saed_sharqawi@hotmail.com>
2019-09-14 21:42:00 +0300

    add the main pages to my personal web site

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,7 +4,9 @@ const ContactPage = () => {
   return (
     <div>
        <h1>Conatct</h1>
-       <p2> the best way to reach me via saed_sharqawi@hotmail.com</p2>
+       <p>the best way to reach me via saed_sharqawi@hotmail.com 
+         <a href="https://github.com/saeeds" target="_blank">github</a>
+        </p>
     </div>
   )
 }
