54e7f1e6e1d4898c288a37ef55edcc4f458b06db
Saeed Alsharqawi <saed_sharqawi@hotmail.com>
2019-09-19 00:29:22 +0300

    Add new theam to my personal web site

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Navbar from './Navbar'
-import Footer from './Footer'
+import Footer from './footer'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './layout.css'
 const Layout = ({ children }) => {
