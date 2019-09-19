2e5905f17903ffe7d02ebaaede447f6a2ddc696a
Saeed Alsharqawi <saed_sharqawi@hotmail.com>
2019-09-19 10:11:11 +0300

    Add error page

--- src/components/SimpleHero.js
+++ src/components/SimpleHero.js
@@ -1,4 +1,5 @@
 import React from 'react'
+
 const SimpleHero = ({ children }) => {
     return (
        <header className="defaultHero">
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react'
 import Layout from '../components/layout'
 import SimpleHero from '../components/SimpleHero'
+import Banner from '../components/Banner'
+import { Link } from 'gatsby'
 export default class index extends Component {
   render() {
     return (
       <Layout>
-        <SimpleHero />
+        <SimpleHero>
+          <Banner title="continue exploring" info="Lorem ispum dolor site amet consecture adipiscicing elit. Eiu, offficies.">
+            <Link to="/projects" className="btn-white">explore projects</Link>
+          </Banner>
+        </SimpleHero>
       </Layout>
     )
   }
