c42db27ffb045a66d385fab5f9b8950aee59d01f
Saeed Alsharqawi <saed_sharqawi@hotmail.com>
2019-09-22 13:48:13 +0300

    Add background image using the gatsby background image plugin

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import Layout from '../components/layout'
 import styles from '../css/error.module.css'
-import { Link } from 'gatsby'
+import AniLink from "gatsby-plugin-transition-link/AniLink";
 import Banner from '../components/Banner'
 const error = () => {
     return (
         <Layout>
             <header className={styles.error}>
                 <Banner title="oops it's a dead end">
-                    <Link to="/" className="btn-white">
+                    <AniLink fade to="/" className="btn-white">
                         back to home page
-               </Link>
+               </AniLink>
                 </Banner>
             </header>
 
