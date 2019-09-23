01d9483068e640d195989b978e7c5116465b2b6a
Saeed Alsharqawi <saed_sharqawi@hotmail.com>
2019-09-23 20:02:16 +0300

    Add the gatsby contentfull  plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,7 @@
 const path = require(`path`)
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
 module.exports = {
   siteMetadata: {
     title: `Said Alsharqawi Personal Site`,
@@ -11,6 +14,13 @@ module.exports = {
       options: {
         name: `images`,
         path: `${__dirname}/src/images/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
       },
     },
     `gatsby-plugin-sass`,
