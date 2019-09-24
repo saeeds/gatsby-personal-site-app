c3b94e9c9f785d8f392a87cdf3fbd7225d8b9b20
Saeed Alsharqawi <saed_sharqawi@hotmail.com>
2019-09-24 11:08:36 +0300

    Add the gatsby contentfull  plugin

--- src/components/Projects/Project.js
+++ src/components/Projects/Project.js
@@ -3,9 +3,25 @@ import Image from 'gatsby-image'
 import styles from '../../css/tour.module.css'
 import { FaMap } from 'react-icons/fa'
 import AniLink from "gatsby-plugin-transition-link/AniLink";
+import PropTypes from 'prop-types'
+import { useStaticQuery, graphql } from 'gatsby'
+
+const getImage = graphql`
+query {
+    file(relativePath:{eq:"defaultBcg.jpeg"}){
+       childImageSharp{
+        fluid{
+            ...GatsbyImageSharpFluid_withWebp
+        }
+      }
+    }
+  }
+`
 const Project = ({ project }) => {
     const { name, slug, months, country, images } = project
-    let mainImage = images[0].fluid
+    const data = useStaticQuery(getImage)
+    const img = data.file.childImageSharp.fluid
+    let mainImage = images ? images[0].fluid : img
     return (
         <article className={styles.tour}>
             <div className={styles.imgContainer}>
@@ -19,7 +35,7 @@ const Project = ({ project }) => {
                 <div className={styles.info}>
                     <h4 className={styles.country}>
                         <FaMap className={styles.icon} />
-                        {country}
+                        {country || 'defualt country'}
                     </h4>
                 </div>
                 <div className={styles.details}>
@@ -31,4 +47,12 @@ const Project = ({ project }) => {
     )
 }
 
+Project.propTypes = {
+    project: PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        country: PropTypes.string.isRequired,
+        months: PropTypes.number.isRequired,
+        images: PropTypes.arrayOf(PropTypes.object).isRequired,
+    })
+}
 export default Project
