3e649d65b585c71b5d179b5ac8a3ae1770694367
Saeed Alsharqawi <saed_sharqawi@hotmail.com>
2019-09-25 15:04:23 +0300

    Add the gatsby contentfull  plugin

--- src/components/Blog/BlogCard.js
+++ src/components/Blog/BlogCard.js
@@ -21,11 +21,12 @@ const BlogCard = ({ blog }) => {
     let mainImage = image ? image.fluid : img
     return (
         <article className={styles.blog}>
-            <div className={styles.imageContainer}>
+            <div className={styles.imgContainer}>
                 <Image fluid={mainImage} className={styles.img} alt={title}/>
                 <AniLink fade className={styles.link} to={`/blog/${slug}`} >read more</AniLink>
+                <h6 className={styles.date}>{published}</h6>
             </div>
-            <h6 className={styles.date}>{published}</h6>
+           
             <div className={styles.footer}>
                 <h4>{title}</h4>
             </div>
